1223a5e8cecc1e4139041b8f884b7b48757d4689
Joako9780 <57472877+Joako9780@users.noreply.github.com>
2025-06-01 16:46:42 -0300

    archivos .py y .R (vid 22 - 26)
    Se crearon los archivos para pre procesado de datos, se importaron las librerías, el dataset y se llenaron los datos faltantes. El anterior commit tuvo que ser revertido.

---
+++ update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/plantilla_procesado_datos_R.R
@@ -0,0 +1,23 @@
+# Plantilla de Pre Procesado de Datos en R
+# Importar el Dataset 
+
+dataset = read.csv("Data.csv")
+
+# Tratamiento de los valores NA
+# con $ se accede a un valor del dataframe
+# dataset$Age es un vector
+# funcion ifelse() para evaluar la condicion de NA en algun valor
+# del dataset y calcular la media o dejar el valor original segun la condicion
+# funcion is.na() evalua si un dato es NA
+# ave() produce un subconjunto de x a partir de las observaciones realizadas
+# na.rm NA Remove, o sea sin los valores NA
+# FUN ahora es una funcion que calcula la media de los x sin tener en cuenta
+# los valores NA
+
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
